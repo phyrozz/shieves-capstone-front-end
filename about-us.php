2861f94da5431c6f5485c000d74cebaaf19bcf4d
Shieves <malijohnvincent17@gmail.com>
2024-05-04 22:34:59 +0800

    added navbar to other pages

--- about-us.php
+++ about-us.php
@@ -8,19 +8,22 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Satisfy&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="tailwind.css">
+    <script src="https://cdn.lordicon.com/lordicon.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/gsap@3.12.5/dist/gsap.min.js"></script>
 </head>
 <body class="bg-slate-800">
-    <div class="m-10 flex flex-col gap-10">
-        <div class="bg-gradient-to-r from-slate-200 to-slate-300 p-10 flex flex-col gap-5 rounded-lg shadow-xl text-center">
+    <?php include "./components/navbar.php"; ?>
+    <div class="mt-28 m-10 flex flex-col gap-10">
+        <div class="bg-gradient-to-t from-slate-300 to-slate-200 p-10 flex flex-col gap-5 rounded-lg shadow-xl text-center">
             <h1 class="font-satisfy text-6xl">About Us</h1>
             <p class="text-sm">Museo De San Pedro is located at #122 Magsasaysay, San Pedro City, Laguna. It's only a 17-minute drive from HBC San Pedro. You can also take a jeepney ride on the "estrella trip" route, heading to Magsasay (beside HBC). Advise the driver to drop you off at "Museo De San Pedro" in front of Villa Consolascion. Alternatively, use the Waze Mobile App to locate us.</p>
         </div>
         <div class="w-full text-center mt-5 mb-0">
             <h1 class="font-satisfy text-4xl font-bold text-slate-50">All In Packages</h1>
         </div>
-        <div class="flex flex-row gap-3 flex-wrap justify-center">
+        <div class="flex flex-row gap-3 flex-wrap justify-center cursor-default">
             <!-- Package containers -->
-            <div class="md:w-96 w-full bg-gradient-to-r from-slate-200 to-slate-300 p-10 flex flex-col gap-5 rounded-lg shadow-xl text-center">
+            <div class="package-container md:w-96 w-full bg-gradient-to-t from-slate-300 to-slate-200 p-10 flex flex-col gap-5 rounded-lg shadow-xl text-center">
                 <!-- Package name, pax, and price -->
                 <div class="flex flex-col gap-1">
                     <p class="font-black text-4xl">PHP 50,000</p>
@@ -63,7 +66,7 @@
                 </div>
             </div>
             <!-- Package containers -->
-            <div class="md:w-96 w-full bg-gradient-to-r from-slate-200 to-slate-300 p-10 flex flex-col gap-5 rounded-lg shadow-xl text-center">
+            <div class="package-container md:w-96 w-full bg-gradient-to-t from-slate-300 to-slate-200 p-10 flex flex-col gap-5 rounded-lg shadow-xl text-center">
                 <!-- Package name, pax, and price -->
                 <div class="flex flex-col gap-1">
                     <p class="font-black text-4xl">PHP 60,000</p>
@@ -105,7 +108,7 @@
                     </div>
                 </div>
             </div>
-            <div class="md:w-96 w-full bg-gradient-to-r from-slate-200 to-slate-300 p-10 flex flex-col gap-5 rounded-lg shadow-xl text-center">
+            <div class="package-container md:w-96 w-full bg-gradient-to-t from-slate-300 to-slate-200 p-10 flex flex-col gap-5 rounded-lg shadow-xl text-center">
                 <!-- Package name, pax, and price -->
                 <div class="flex flex-col gap-1">
                     <p class="font-black text-4xl">PHP 70,000</p>
@@ -146,9 +149,77 @@
                         </ul>
                     </div>
                 </div>
-                
-        
-        <p class="text-white">Contact us at <a class="hover:underline font-bold" href="mailto:info@museodesanpedro.com">info@museodesanpedro.com</a>.</p>
+            </div>
+        </div>
+        <p class="text-white text-center m-auto">Contact us at <a class="hover:underline font-bold" href="mailto:info@museodesanpedro.com">info@museodesanpedro.com</a></p>
     </div>
+    <script>
+        document.getElementById("login-btn").addEventListener("mouseenter", () => {
+            var tl = new TimelineMax({ paused: true });
+            tl.from("#login-btn-icon", {x: 50, duration: 0.20 })
+            tl.from("#login-btn-text", { opacity: 0, x: 20, duration: 0.25 });
+
+            if (!tl.isActive()) {
+                tl.play(0);
+            }
+        });
+        document.querySelectorAll('.package-container').forEach(item => {
+            item.addEventListener('mouseenter', () => {
+                onPackageItemHoverIn(item);
+            });
+        });
+        document.querySelectorAll('.package-container').forEach(item => {
+            item.addEventListener('mouseleave', () => {
+                onPackageItemHoverOut(item);
+            });
+        });
+        document.querySelectorAll('.navbar-item').forEach(item => {
+            item.addEventListener('mouseenter', () => {
+                bounceNavbarItem(item);
+            });
+        });
+
+        function bounceNavbarItem(target) {
+            var tl = new TimelineMax({ paused: true });
+            tl.to(target, { y: -20, duration: 0.1, ease: "easeIn" })
+            .to(target, { y: 0, duration: 0.1, ease: "easeOut" });
+
+            if (!tl.isActive()) {
+                tl.play(0);
+            }
+        };
+        function onPackageItemHoverIn(target) {
+            var tl = new TimelineMax({ paused: true });
+            tl.to(target, { scale: 1.05, duration: 0.1, ease: "easeIn" });
+
+            if (!tl.isActive()) {
+                tl.play(0);
+            }
+        };
+
+        function onPackageItemHoverOut(target) {
+            var tl = new TimelineMax({ paused: true });
+            tl.to(target, { scale: 1, duration: 0.25, ease: "easeOut" });
+
+            if (!tl.isActive()) {
+                tl.play(0);
+            }
+        };
+
+        let prevScrollpos = window.pageYOffset;
+        window.onscroll = function () {
+            let currentScrollPos = window.pageYOffset;
+            let scrollDownText = document.getElementById("scroll-down-text");
+            let navbar = document.getElementById("navbar");
+
+            if (prevScrollpos > currentScrollPos) {
+                navbar.style.top = "0";
+            } else {
+                navbar.style.top = "-75px";
+            }
+
+            prevScrollpos = currentScrollPos;
+        };
+    </script>
 </body>
 </html>
---
+++ components/navbar.php
@@ -0,0 +1,48 @@
+
+
+<nav id="navbar" class="flex flex-nowrap gap-8 bg-slate-300 flex-row w-full py-2 px-5 items-center fixed justify-center md:justify-between top-0 z-50 transition-all">
+    <h1 class="font-extrabold text-lg md:block hidden">Museo de San Pedro</h1>
+    <ul class="flex flex-row gap-5 items-center">
+        <a href="/index.php" class="navbar-item hover:bg-slate-400 font-bold text-sm p-3 rounded-md transition-all text-center">
+            <li class="flex flex-row items-center gap-2"> 
+                <lord-icon
+                    src="https://cdn.lordicon.com/wmwqvixz.json"
+                    trigger="hover"
+                    style="width:1.5em;height:1.5em">
+                </lord-icon>
+                <p class="navbar-item-text">HOME</p>
+            </li>
+        </a>
+        <a href="/bookings.php" class="navbar-item hover:bg-slate-400 font-bold text-sm p-3 rounded-md transition-all text-center">
+            <li class="flex flex-row items-center gap-2"> 
+                <lord-icon
+                    src="https://cdn.lordicon.com/wmlleaaf.json"
+                    trigger="hover"
+                    style="width:1.5em;height:1.5em">
+                </lord-icon>
+                <p class="navbar-item-text">BOOK NOW</p>
+            </li>
+        </a>
+        <a href="/about-us.php" class="navbar-item hover:bg-slate-400 font-bold text-sm p-3 rounded-md transition-all text-center">
+            <li class="flex flex-row items-center gap-2">
+                <lord-icon
+                    src="https://cdn.lordicon.com/jnzhohhs.json"
+                    trigger="hover"
+                    style="width:1.5em;height:1.5em">
+                </lord-icon>
+                <p class="navbar-item-text">ABOUT US</p>
+            </li>
+        </a>
+        <a id="login-btn" href="/admin.html" class="hover:bg-slate-400 font-semibold p-3 rounded-md transition-all text-center group">
+            <li class="flex flex-row items-center gap-2">
+                <lord-icon
+                    id="login-btn-icon"
+                    src="https://cdn.lordicon.com/hrjifpbq.json"
+                    trigger="hover"
+                    style="width:2em;height:2em">
+                </lord-icon>
+                <p id="login-btn-text" class="group-hover:block hidden">LOG IN</p>
+            </li>
+        </a>
+    </ul>
+</nav>
