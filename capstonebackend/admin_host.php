1faf54a58e98c358c5e8c4d0c6f8fe278269f7cf
Shieves <malijohnvincent17@gmail.com>
2024-05-11 15:01:55 +0800

    merged backend to the project

---
+++ capstonebackend/admin_host.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin Home</title>
+    <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+    
+     <div class="container">
+
+        <div class="content">
+            <h3>Hi, <span>User</span></h3>
+            <h1>Welcome <span></span></h1>
+            <p>This is an Account Creation page</p>
+            <a href= "login.php" class="btn">Login</a>
+            <a href= "register_form.php" class="btn">Register now</a>
+            <a href= "logout.php" class="btn">Logout</a>
+
+</body>
+</html>
---
+++ capstonebackend/login.php
@@ -0,0 +1,92 @@
+<?php
+
+$host="localhost";
+$user="root";
+$password="";
+$db="user";
+
+$data=mysqli_connect($host,$user,$password,$db);
+if($data===false)
+{
+    die("connection error");
+}
+
+if($_SERVER["REQUEST_METHOD"]=="POST")
+{
+    $username=$_POST["username"];
+    $password=$_POST["password"];
+
+    $sql= "SELECT * FROM login WHERE username= '".$username."' AND password= '".$password."' ";
+
+    $result=mysqli_query($data, $sql);
+
+    $row=mysqli_fetch_array($result);
+
+    if($row["usertype"]=="user")
+    {
+        header("location:user_home.php");
+    }
+
+    elseif($row["usertype"]=="admin")
+    {
+        header("location:admin_home.php");
+    }
+
+    else
+    {
+        echo "username or password incorrect";
+    }
+}
+
+
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Museo de San Pedro Admin</title>
+    
+    <!-- custom css file link -->
+    <link rel="stylesheet" href="style.css">
+    
+
+     <center>
+
+          <h1>Welcome to Museo de San Pedro!</h1>
+          <br><br><br><br>
+          <div style="background-color: lightblue; width: 500px;">
+          <br><br>
+
+          <form action="#" method="POST">
+
+     <div>
+         <label>Username</label>
+         <input type="text" name="username" required>
+     </div> 
+     <br><br>
+     
+     <div>
+         <label>Password</label>
+         <input type="text" name="password" required>
+     </div> 
+     <br><br>
+     
+     <div>
+
+         <input type="submit" value="login" required>
+     </div>
+    <br><br>
+    <p>Go Back to <a href= "admin_host.php" class="btn">Admin Page</a></p>
+     </center>
+
+</head>
+<body>
+
+    
+
+</body>
+</html>
